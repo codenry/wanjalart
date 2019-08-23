1d81fe5b718768ddabe6223b85262ec2c7b8f064
henry.wanjala.dev <henry.wanjala.dev@henrywanjaladevs-MacBook-Pro.local>
2019-08-23 17:10:12 -0400

    add scroll animation

--- wanjalapaints/ViewController.swift
+++ wanjalapaints/ViewController.swift
@@ -11,9 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var paintingsCollectionView: UICollectionView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.delegate = self
         paintingsCollectionView.delegate = self
         paintingsCollectionView.dataSource = self
     }
@@ -35,9 +38,50 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
         return cell
     }
     
-    
+
 }
 
 
-
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        if offsetY < 0 {
+           //coder here in the future
+            
+        }
+        if let paintingsCollectionView = scrollView as? UICollectionView {
+            for cell in paintingsCollectionView.visibleCells as! [SectionCollectionViewCell] {
+                let indexPath = paintingsCollectionView.indexPath(for: cell)!
+                let attributes = paintingsCollectionView.layoutAttributesForItem(at: indexPath)!
+                let cellFrame = paintingsCollectionView.convert(attributes.frame, to: view)
+                let translationX = cellFrame.origin.x / 5
+                cell.coverImageView.transform = CGAffineTransform(translationX: translationX, y: 0)
+                cell.layer.transform = animateCell(cellFrame: cellFrame)
+                
+            }
+            
+        }
+    }
+    
+    func animateCell(cellFrame: CGRect) -> CATransform3D {
+        let angleFromX = Double((-cellFrame.origin.x) / 10)
+        let angle = CGFloat((angleFromX * Double.pi) / 180.0)
+        var transform = CATransform3DIdentity
+        transform.m34 = -1.0/1000
+        let rotation = CATransform3DRotate(transform, angle, 0, 1, 0)
+        
+        var scaleFromX = (1000 - (cellFrame.origin.x - 200)) / 1000
+        let scaleMax: CGFloat = 1.0
+        let scaleMin: CGFloat = 0.6
+        if scaleFromX > scaleMax {
+            scaleFromX = scaleMax
+        }
+        if scaleFromX < scaleMin {
+            scaleFromX = scaleMin
+        }
+        let scale = CATransform3DScale(CATransform3DIdentity, scaleFromX, scaleFromX, 1)
+        
+        return CATransform3DConcat(rotation, scale)
+    }
+}
 
