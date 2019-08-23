16e3b1800e8581b4dd97f9466f2b199866240162
henry.wanjala.dev <henry.wanjala.dev@henrywanjaladevs-MacBook-Pro.local>
2019-08-22 22:09:48 -0400

    parse data to collection view

---
+++ wanjalapaints/SectionCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SectionCollectionViewCell.swift
+//  wanjalapaints
+//
+//  Created by henry.wanjala.dev on 2019-08-22.
+//  Copyright © 2019 henry.wanjala.dev. All rights reserved.
+//
+
+import UIKit
+
+class SectionCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var coverImageView: UIImageView!
+    
+    
+}
